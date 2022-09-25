4cec4b852668dba8c54d6a6423871107c942184f
jeffersonnobrega <jeffersonnobrega@gmail.com>
2022-09-25 20:47:39 -0300

    adicionando informações das aulas sobre variaveis

---
+++ JAVA/Bootcamp-Quebec-Dio/IntelliJ-workspace/Primeiro_Programa_IntellJ/src/br/com/dio/variaveis/main.java
@@ -0,0 +1,20 @@
+package br.com.dio.variaveis;
+
+
+import java.sql.SQLOutput;
+
+public class main {
+    public static void main(String[] args) {
+        byte b = 15;
+        short s = -15785;
+        int i = 8515785;
+        long l = 29583157L; //long tem que colocar a letra L no final
+        float f = 3.14f;
+        double d = 3.14d;// não precisa do d se ficar sem d será entendido como double
+        char c = 'T'; //char e somente um caracter, acima disso dá erro
+        String s = "T";
+        boolean s = "false";
+
+        System.out.println(b);
+    }
+}
