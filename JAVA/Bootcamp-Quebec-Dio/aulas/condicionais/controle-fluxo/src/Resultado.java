bfb2135dd85cde370a5bf48cff0efc74800bde83
Jefferson Nobrega da Silva <u95074@brbservicos.com.br>
2022-10-25 15:55:19 -0300

    adicionando arquivos da aula

--- JAVA/Bootcamp-Quebec-Dio/aulas/condicionais/controle-fluxo/src/Resultado.java
+++ JAVA/Bootcamp-Quebec-Dio/aulas/condicionais/controle-fluxo/src/Resultado.java
@@ -1,12 +1,16 @@
-public class ResultaoEscolar {
+public class Resultado{
     public static void main(String[] args) {
         int nota = 6;
 
         if(nota >=7)
         System.out.println("Aprovado");
 
+        else if (nota > 5 && nota < 7)
+        System.out.println("Está de recuperação");
+
         else
         System.out.println("Reprovado");
         //quando tiver mais de uma instrução dentro do if ou do else é preciso colocar {}
     }
 }
+
---
+++ JAVA/Bootcamp-Quebec-Dio/aulas/condicionais/controle-fluxo/src/condicao_ternaria.java
@@ -0,0 +1,13 @@
+public class condicao_ternaria {
+    public static void main(String[] args) {
+
+        int nota = 5;
+
+        String resultado = nota >=7 ? "Aprovado" : "Reprovado";    
+
+        //ele atribiu o resultado da pergunta "se nota é igual o maior que 7" a variável resultado.
+        // : significa else
+
+        System.out.println(resultado);
+    }
+}
